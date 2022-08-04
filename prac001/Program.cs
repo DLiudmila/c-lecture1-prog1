397b50315a9eded7aeecc932dfab5b70bb651c74
Liudmila D <verliebtin@mail.ru>
2022-08-04 22:52:11 +0300

    prac1

---
+++ prac001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine(b + " Является квадратом " + a);
+}
+else
+{
+    Console.WriteLine(b + " Не является квадратом " + a);
+}
---
+++ prac002/Program.cs
@@ -0,0 +1,40 @@
+﻿int d = 0;
+// while(d > 0 || d < 8) 
+// {
+    Console.WriteLine(" Введите число от 1 до 7");
+    d = Convert.ToInt32(Console.ReadLine());
+// }
+
+if (d == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (d == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (d == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (d == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (d == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (d == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (d == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if ( d < 1 || d > 7)
+{
+    Console.WriteLine("Сам дурак");
+}
+
